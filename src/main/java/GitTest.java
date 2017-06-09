57342989fff5d8189182c2429f47e9d2f184a8cc
siruan <451368604@qq.com>
2017-06-09 15:29:06 +0800

    添加version方法

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,4 +5,11 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("GitTest.main");
     }
+
+    public void version ()
+    {
+        String version;
+        version = "1.0";
+        System.out.println("version="+version);
+    }
 }
